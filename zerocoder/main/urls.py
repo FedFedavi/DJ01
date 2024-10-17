10996a5919fb4a61ce85d6563bfb989a2bcc0c56
FedFedavi <kykypyk@gmail.com>
2024-10-17 20:37:07 +0400

    add html $ code dz

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
+    path('page3/', views.page3, name='page3'),
+    path('page4/', views.page4, name='page4'),
 
 ]
