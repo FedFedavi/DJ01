4c02ca301d939b8efd8d248b0e6fa57e82d617bb
FedFedavi <kykypyk@gmail.com>
2024-10-19 11:09:21 +0400

    add new app News

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     data = {
-        'caption':"Django"
+        'caption': "Django"
     }
     return render(request, "main/index.html", data)
 
