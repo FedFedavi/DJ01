e86ea6a20b3b4a0e07f069b9eaf066fa7d1bb2eb
FedFedavi <kykypyk@gmail.com>
2024-10-20 17:46:22 +0400

    add forms html & code

--- zerocoder/news/urls.py
+++ zerocoder/news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='news_home'),
     path('dz/', views.home_dz, name='news_dz'),
+    path('add_news/', views.add_news, name='add_news'),
 ]
--- zerocoder/news/views.py
+++ zerocoder/news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News_post
 from .models import Dz_post
+from .forms import News_postForm
 
 # Create your views here.
 
@@ -13,3 +14,17 @@ def home(request):
 def home_dz(request):
     news = Dz_post.objects.all()
     return render(request, 'news/news.html', {'news_dz': news})
+
+
+def add_news(request):
+    error = ""
+    if request.method == 'POST':
+        form = News_postForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Данные внесены некорректно'
+
+    form = News_postForm()
+    return render(request, 'news/add_news.html', {'form': form, 'errors': error})
